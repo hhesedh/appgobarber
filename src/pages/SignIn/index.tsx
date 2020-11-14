765a31a2f3e2c7c3a53f846a28f862f790deb66d
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2020-11-14 18:17:56 -0300

    criação de input e botão

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Image, Text } from "react-native";
+import { Image } from "react-native";
 
 import logoImg from "../../../assets/img/logo/logo.png";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
 import { Container, Title } from "./styles";
 
 const SignIn: React.FC = () => {
@@ -9,6 +11,16 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title> Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button
+        onPress={() => {
+          console.log("entrar");
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
